d380047bf947e66c9e7d3d6857bc0dfd5120c7d3
App Academy Student <student@appacademy.io>
2015-04-24 11:03:25 -0700

    Finish Phases II and III

--- 00_tree_node.rb
+++ 00_tree_node.rb
@@ -12,4 +12,13 @@ class PolyTreeNode
     @parent = new_parent
     parent.children << self unless parent.nil? || parent.children.include?(self)
   end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Node is not a child." unless children.include?(child_node) 
+    child_node.parent = nil
+  end
 end
