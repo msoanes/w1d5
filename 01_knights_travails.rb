e31d661eef5b32f9db1d21cde19d33a419776a6c
App Academy Student <student@appacademy.io>
2015-04-24 13:45:11 -0700

    Complete Knight's Travails Phase II, build_move_tree

--- 01_knights_travails.rb
+++ 01_knights_travails.rb
@@ -6,33 +6,45 @@ class KnightPathFinder
   def initialize(starting_pos = [0, 0])
     @starting_pos = PolyTreeNode.new(starting_pos)
     @visited_poss = [starting_pos]
+
   end
 
   def self.valid_moves(pos)
-    moves = [1,-1,2,-2].permutation(2).to_a
+    moves = [1, -1, 2, -2].permutation(2).to_a
     moves.reject! { |move| move.first.abs == move.last.abs }
     new_poss = moves.map do |move|
       [pos.first + move.first, pos.last + move.last]
     end
     new_poss.select do |new_pos|
-      (0..7).include?(new_pos.first) && \
+      (0..7).include?(new_pos.first) &&
         (0..7).include?(new_pos.last)
     end
+
   end
 
   def build_move_tree
+    queue = [starting_pos]
+    until queue.empty?
+      current_pos = queue.shift
+      new_move_poss(current_pos).each do |new_move_pos|
+        new_pos = PolyTreeNode.new(new_move_pos)
+        new_pos.parent = current_pos
+        queue << new_pos
+      end
+    end
   end
 
   def new_move_poss(pos)
     new_poss = self.class.valid_moves(pos.value)
     new_poss.reject! { |new_pos| visited_poss.include?(new_pos) }
-    visited_poss += new_poss
+    visited_poss.concat(new_poss)
     new_poss
   end
 end
 
 # Basic testing
 
-p KnightPathFinder.valid_moves([4,4])
 
 kpf = KnightPathFinder.new([0, 0])
+
+kpf.build_move_tree
