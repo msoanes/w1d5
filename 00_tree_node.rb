0895e90f02abbd82a9612101c53835339cb68d2e
App Academy Student <student@appacademy.io>
2015-04-24 11:27:17 -0700

    Complete Phase IV and PolyTreeNode class

--- 00_tree_node.rb
+++ 00_tree_node.rb
@@ -18,7 +18,28 @@ class PolyTreeNode
   end
 
   def remove_child(child_node)
-    raise "Node is not a child." unless children.include?(child_node) 
+    raise "Node is not a child." unless children.include?(child_node)
     child_node.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if value == target_value
+    children.each do |child|
+      child_search_result = child.dfs(target_value)
+      return child_search_result if child_search_result
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+      queue += current_node.children
+    end
+
+    nil
   end
 end
